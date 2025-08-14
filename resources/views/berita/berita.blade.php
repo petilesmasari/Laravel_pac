782532d94acdf8b5c05c4248cdd2205b19df7108
user <petilesmasari>
2025-08-14 22:55:48 +0700

    ubah berita

--- resources/views/berita/berita.blade.php
+++ resources/views/berita/berita.blade.php
@@ -14,7 +14,7 @@
                     <div class="card border-0 shadow">
                         <img src="{{ asset('storage/artikel/' . $item->image)}}" alt="Berita">
                         <div class="px-2 py-3">
-                            <p class="mb-3">{{$item->created_at}}</p>
+                            <p class="mb-3">{{ $item->created_at->timezone('Asia/Jakarta')->format('Y-m-d H:i:s') }}</p>
                             <h4 class="mb-3 font-bold">{{$item->judul}}</h4>
                             <a href="/detail/{{ $item->slug }}" class="mb-3 text-danger text-decoration-none">Selengkapnya</a>
                         </div>
--- resources/views/berita/detail.blade.php
+++ resources/views/berita/detail.blade.php
@@ -7,7 +7,7 @@
         <div class="mb-3"> <a href="{{route('home')}}">Beranda</a> / <a href="{{route('berita')}}">Berita</a> / Berita Kegiatan Pontianak Archery Club</div>
         <img src="{{ asset('storage/artikel/' . $artikel->image)}}" alt="Berita 1" class="img-fluid">
         <div class="konten-berita px-2 py-3">
-            <p class="mb-3">{{$artikel->created_at}}</p>
+            <p class="mb-3">{{$artikel->created_at->timezone('Asia/Jakarta')->format('Y-m-d H:i:s') }}</p>
             <h4 class="mb-3 font-bold">{{$artikel->judul}}</h4>
             <p class="text-muted">{!! $artikel->desc !!}</p>
         </div>
